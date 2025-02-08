02549234c1e0c122de2d5fa3558e857336a7ad0d
sixdgt <c4crypt@gmail.com>
2025-02-08 10:29:18 +0545

    ImageIcon loading using method

--- SwingComponent/src/ui/panel/TopPanel.java
+++ SwingComponent/src/ui/panel/TopPanel.java
@@ -17,20 +17,20 @@ public class TopPanel extends JPanel{
         user = new JMenu("Users", true);
         setting = new JMenu("Settings", true);
         // to fetch image from source
-        ImageIcon add_icon = new ImageIcon(TopPanel.class.getResource("/icons/add-user.png"));
-        ImageIcon show_icon = new ImageIcon(TopPanel.class.getResource("/icons/group.png"));
-        ImageIcon signout_icon = new ImageIcon(TopPanel.class.getResource("/icons/logout.png"));
-        ImageIcon user_icon = new ImageIcon(TopPanel.class.getResource("/icons/user.png"));
-        // to resize image
-        Image resize_add = add_icon.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH);
-        Image resize_show = show_icon.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH);
-        Image resize_signout = signout_icon.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH);
-        Image resize_user = user_icon.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH);
-        
-        user_list = new JMenuItem("Show User", new ImageIcon(resize_show));
-        user_add = new JMenuItem("Add User", new ImageIcon(resize_add)); // adding image icon
-        profile = new JMenuItem("Profile", new ImageIcon(resize_user));
-        logout = new JMenuItem("Logout", new ImageIcon(resize_signout));
+//        ImageIcon add_icon = new ImageIcon(TopPanel.class.getResource("/icons/add-user.png"));
+//        ImageIcon show_icon = new ImageIcon(TopPanel.class.getResource("/icons/group.png"));
+//        ImageIcon signout_icon = new ImageIcon(TopPanel.class.getResource("/icons/logout.png"));
+//        ImageIcon user_icon = new ImageIcon(TopPanel.class.getResource("/icons/user.png"));
+//        // to resize image
+//        Image resize_add = add_icon.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH);
+//        Image resize_show = show_icon.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH);
+//        Image resize_signout = signout_icon.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH);
+//        Image resize_user = user_icon.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH);
+//        
+        user_list = new JMenuItem("Show User", new ImageIcon(loadImage("/icons/group.png", 20, 20)));
+        user_add = new JMenuItem("Add User", new ImageIcon(loadImage("/icons/add-user.png", 20, 20))); // adding image icon
+        profile = new JMenuItem("Profile", new ImageIcon(loadImage("/icons/user.png", 20, 20)));
+        logout = new JMenuItem("Logout", new ImageIcon(loadImage("/icons/logout.png", 20, 20)));
         
         user.add(user_list, 0);
         user.add(user_add, 1);
@@ -40,5 +40,16 @@ public class TopPanel extends JPanel{
         jmb.add(user);
         jmb.add(setting);
         add(jmb, new BorderLayout().NORTH);
+    }
+    // to load the image from specified url
+    public final Image loadImage(String url, int width, int height){
+        Image img = null;
+        try {
+            ImageIcon icon = new ImageIcon(TopPanel.class.getResource(url));
+            img = icon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        } catch (Exception e) {
+            System.out.println("No image found");
+        }
+        return img;
     }
 }
